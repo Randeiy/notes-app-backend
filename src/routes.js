95733f536625eb36d24b9cc4bca3afe085726bc6
Randy Ahmad Garcia Santosa <randyahmad200@gmail.com>
2022-10-27 12:46:32 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line object-curly-newline
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
